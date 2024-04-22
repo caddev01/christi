5c3e6fb48088e44d7c9195a5ba50292bdec0de1b
Nuel Ademigoke <mademigoke@gmail.com>
2024-04-22 16:00:18 +0100

    Updated the User button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
 
 const Header = () => {
     return(
@@ -29,15 +30,24 @@ const Header = () => {
                 <a className="nav-link" href="">The Church</a>
                 </li>
             </ul> 
-            <div className='col-1 text-center'>
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#e85a4f" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                </svg> */}
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#e85a4f" class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                </svg>
+            {/* User button */}
+            <div className='col-1 text-center'>
+                <Dropdown>
+                    <Dropdown.Toggle variant="" id="dropdown-basic">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#e85a4f" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className='text-center bg-dark'>
+                        <Dropdown.Item className='text-light' href="#/action-1">Sign in</Dropdown.Item>
+                        <Dropdown.Item className='text-light' href="#/action-2">Logout</Dropdown.Item>
+                        <Dropdown.Item className='text-light' href="#/action-3">Create account</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                
             </div>
         </div>
 
