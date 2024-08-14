c74218608b31bf5c709883e705918f9d25f0d321
Nuel Ademigoke <mademigoke@gmail.com>
2024-08-14 12:20:04 +0100

    Continued the Heroes Quizer

--- src/Components/Gamedeck.js
+++ src/Components/Gamedeck.js
@@ -2,83 +2,120 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import BottomCanvas from '../Components/BottomCanvas';
+import questions from './Quizer/HeroesQuestions';
 
-const Gamedeck = () => {
+
+function Gamedeck () {
+    
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [showText, setShowText] = useState(false);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [startQuizer, setStartQuizer] = useState(false);
+
+    const handleStartQuizer = () => {
+        setStartQuizer(true) && setCurrentQuestion(1) 
+    };
+
+    const handleAnswerOptionClick = (selectedAnswer) => {
+        selectedAnswer === questions[currentQuestion].correctAnswer && setScore(score + 1);
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length){
+            setCurrentQuestion(nextQuestion);
+        }else{
+            setShowScore(true);
+            score == questions.length - 1 && setShowText(true);
+        }
+    };
+
+    const handleRestartQuizer = () => {
+        setScore(0); setShowScore(false); setShowText(false); setCurrentQuestion(0); setStartQuizer(false);
+    }
+    
     return(
 
         <div>
-            {/* //Start Quizer */}
-            <div className='d-flex text-center'>
-                <div class="d-flex row align-items-center justify-contents-center mt-3 mb-2 mx-auto">
-                    <div class="flex-shrink-0 col-md">
-                        <img src="https://i.pinimg.com/236x/31/40/bb/3140bbe68d99cb693a1fc7a950d9d035.jpg" alt="..." className="img-fluid rounded" />
+            {/* Start Quizer */}
+            {startQuizer ? (
+                /* Question deck */
+                <div className="container border border-secondary rounded mx-auto mt-5 h-100 mb-5" style={{width:'70%'}}>
+                <div className='row'>
+                    {/* Question Card */}
+                    <div className='col-md-8'>
+                        <div className="card mb-3 mt-2" style={{maxWidth: '700px'}}>
+                            <div className="row g-0">
+                                <div className="col-md-7">
+                                <img src="https://i.pinimg.com/564x/9b/3e/76/9b3e76280ec462ea5bc10037e1e23d3c.jpg" className="img-fluid rounded-start" alt="..." />
+                                </div>
+                                <div className="col-md-5">
+                                <div className="card-body">
+                                    <h5 className="card-title">question</h5>
+                                    <p className="card-text">{questions[currentQuestion].question}</p>
+                                    <p className="card-text"><small className="text-body-secondary">not a disciple or prophet</small></p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="flex-grow-1 col-md ms-3">
-                        <h2 className='josefin-sans mt-2'>Heroes</h2>
-                        <button className='btn btn-danger josefin-sans'>Begin</button>
+
+                    {/* Response elements */}
+                    <div className='col-md-4 d-flex align-items-center justify-content-sm-center'>
+                        <div className='d-flex mb-3 justify-content-center align-items-center vstack'>
+
+                            {/* Timer */}
+                            <div className='card-group' style={{minWidth:'170px'}}>
+                                <div className="card text-center mt-2">
+                                    <div className="card-header">
+                                        Timer
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">30s</h5>
+                                    </div>
+                                    <div className="card-footer text-body-secondary">
+                                        Points
+                                    </div>
+                                </div>
+
+                                <div className="card text-center mt-2">
+                                    <div className="card-header">
+                                        Score
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">5</h5>
+                                    </div>
+                                    <div className="card-footer text-body-secondary">
+                                        3 more
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='mt-1'><BottomCanvas /></div>                      
+
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                </div>
+            ):(
+                <div className='d-flex text-center'>
+                    <div className="d-flex row align-items-center justify-contents-center mt-3 mb-2 mx-auto">
+                        <div class="flex-shrink-0 col-md">
+                            <img src="https://i.pinimg.com/236x/31/40/bb/3140bbe68d99cb693a1fc7a950d9d035.jpg" alt="..." className="img-fluid rounded" />
+                        </div>
+                        <div class="flex-grow-1 col-md ms-3">
+                            <h2 className='josefin-sans mt-2'>Heroes</h2>
+                            <button 
+                            className='btn btn-danger josefin-sans'
+                            onClick={()=> handleStartQuizer()} 
+                            >Begin</button>
+                        </div>
+                    </div>
+                </div>
+            )}
             
-            {/* // Question deck */}
-        <div className="container border border-secondary rounded mx-auto mt-5 h-100 mb-5" style={{width:'70%'}}>
-            <div className='row'>
-                {/* Question Card */}
-                <div className='col-md-8'>
-                    <div className="card mb-3 mt-2" style={{maxWidth: '700px'}}>
-                        <div className="row g-0">
-                            <div className="col-md-7">
-                            <img src="https://i.pinimg.com/564x/9b/3e/76/9b3e76280ec462ea5bc10037e1e23d3c.jpg" className="img-fluid rounded-start" alt="..." />
-                            </div>
-                            <div className="col-md-5">
-                            <div className="card-body">
-                                <h5 className="card-title">Philistine Giant</h5>
-                                <p className="card-text">He fought Goliath with a sling and five smooth stones, the weapons of a shepherd boy. He wins, and Goliath is slain.</p>
-                                <p className="card-text"><small className="text-body-secondary">not a disciple or prophet</small></p>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            
+            
 
-                {/* Response elements */}
-                <div className='col-md-4 d-flex align-items-center justify-content-sm-center'>
-                    <div className='d-flex mb-3 justify-content-center align-items-center vstack'>
-
-                        {/* Timer */}
-                        <div className='card-group' style={{minWidth:'170px'}}>
-                            <div className="card text-center mt-2">
-                                <div className="card-header">
-                                    Timer
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">30s</h5>
-                                </div>
-                                <div className="card-footer text-body-secondary">
-                                    Points
-                                </div>
-                            </div>
-
-                            <div className="card text-center mt-2">
-                                <div className="card-header">
-                                    Score
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title">5</h5>
-                                </div>
-                                <div className="card-footer text-body-secondary">
-                                    3 more
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='mt-1'><BottomCanvas /></div>                      
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
 
         </div>
 
---
+++ src/Components/Quizer/HeroesQuestions.js
@@ -0,0 +1,42 @@
+//Questions database
+
+const questions = [
+    {
+        question: "He fought Goliath with a sling and five smooth stones, the weapons of a shepherd boy. He wins, and Goliath is slain.",
+        title: "Arrogant Giant",
+        options: ["Moses", "Abel", "Mary", "David"],
+        correctAnswer: "David",
+        images: [
+            "https://i.pinimg.com/236x/4b/79/2a/4b792a6355efb9e11c97b9b4d5faa750.jpg",
+            "https://i.pinimg.com/236x/33/32/b6/3332b6ca0dcb0473041c8fa24648eadc.jpg",
+            "https://i.pinimg.com/236x/67/35/c6/6735c67d2df671a50cbb4bcfd26274e3.jpg",
+            "https://i.pinimg.com/236x/e2/95/22/e29522e023752f55ff5bbb798030f3d1.jpg",
+        ],
+    },
+    {
+        question: "When a philistian was found bullying an Israelite he killed the philistine to avenge his tribesman",
+        title: "Holly Kill",
+        options: ["Mary", "Moses", "David", "Abel"],
+        correctAnswer: "Moses",
+        images: [
+            "https://i.pinimg.com/236x/67/35/c6/6735c67d2df671a50cbb4bcfd26274e3.jpg",
+            "https://i.pinimg.com/236x/4b/79/2a/4b792a6355efb9e11c97b9b4d5faa750.jpg",
+            "https://i.pinimg.com/236x/e2/95/22/e29522e023752f55ff5bbb798030f3d1.jpg",
+            "https://i.pinimg.com/236x/33/32/b6/3332b6ca0dcb0473041c8fa24648eadc.jpg",
+        ],
+    },
+    {
+        question: "At the begining when men started to give sacrifices to God, he offered a very gratifying sacrifice to God but was murdered due to jealousy",
+        title: "Kinslayer",
+        options: ["David", "Mary", "Abel", "Moses"],
+        correctAnswer: "Abel",
+        images: [
+            "https://i.pinimg.com/236x/e2/95/22/e29522e023752f55ff5bbb798030f3d1.jpg",
+            "https://i.pinimg.com/236x/67/35/c6/6735c67d2df671a50cbb4bcfd26274e3.jpg",
+            "https://i.pinimg.com/236x/33/32/b6/3332b6ca0dcb0473041c8fa24648eadc.jpg",
+            "https://i.pinimg.com/236x/4b/79/2a/4b792a6355efb9e11c97b9b4d5faa750.jpg",
+        ],
+    }
+];
+
+export default questions;
